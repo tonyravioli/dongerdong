f5a7c78fbbd9906451876dfc5761a9299308447f
tonyravioli <tonyravioli187@gmail.com>
2016-04-14 17:00:39 -0400

    Oh, look, modular commands.

---
+++ extcmd/conspiracy.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import markovify.text
+
+
+def doit(sentences=2):
+  import markovify.text
+
+  logfile = "../../conspiradump.txt"
+
+  with open(logfile) as f:
+    text = f.read()
+
+  model = markovify.text.NewlineText(text, state_size=3)
+  longstring = ''
+
+  for i in range(sentences):
+    sent = model.make_sentence(tries=50)
+    try:
+      if sent.endswith("." or "?"):
+        sent = "{} ".format(sent)
+      else:
+        sent = "{}. ".format(sent)
+      longstring = "{}{}".format(longstring,sent)
+    except AttributeError:
+      continue
+
+  return longstring
