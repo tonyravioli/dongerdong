dfeb9f0c8d52cbe91344a2b115a3524469373a3b
Ramiro Bou <bouramiro@gmail.com>
2016-09-02 16:50:48 -0300

    Adding an !update extcmd

---
+++ extcmd/update.py
@@ -0,0 +1,25 @@
+import subprocess
+import os
+import sys
+import time
+
+helptext = "Updates and restarts the bot"
+adminonly = True
+def doit(irc, target, source):
+    commands = ["git fetch",
+                "git rebase --stat --preserve-merges"]
+    
+    for command in commands:
+        child = subprocess.Popen(command.split(),
+                                 stdout=subprocess.PIPE,
+                                 stderr=subprocess.PIPE)
+        (out, err) = child.communicate()
+        ret = child.returncode
+        
+        irc.message(source, "{0} returned code {1}".format(command, ret))
+        for line in (out + err).splitlines():
+            irc.message(source, line.decode("utf-8"))
+    
+
+    irc.eventloop.schedule(irc.quit, "Updating...")
+    irc.eventloop.schedule(os.execl, sys.executable, sys.executable, *sys.argv)
