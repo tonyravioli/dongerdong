8c68ed5e78086e4fabfcd4a70625898efffa2909
tonyravioli <tonyravioli187@gmail.com>
2016-10-21 09:52:48 -0400

    Explicitly reading as utf-8. Closes #75

--- extcmd/dong.py
+++ extcmd/dong.py
@@ -3,6 +3,6 @@ import json
 import random
 helptext = "Produces a genuine, authentic donger"
 
-dongerlist = json.load(open("wisdom/dongers.json"))
+dongerlist = json.load(open("wisdom/dongers.json", 'r', encoding='utf-8'))
 def doit(irc, target, source):
   irc.message(target, random.choice(dongerlist))
--- extcmd/jaden.py
+++ extcmd/jaden.py
@@ -3,6 +3,6 @@ import json
 import random
 helptext = "Outputs a random tweet from Jaden Smith."
 
-jadenlist = json.load(open("wisdom/jaden.json"))
+jadenlist = json.load(open("wisdom/jaden.json", 'r', encoding='utf-8'))
 def doit(irc, target, source):
   irc.message(target, random.choice(jadenlist))
